0b9dd6d322d58051fa4b8222e69624bd5fad0ed2
gnc <chidieberen1999@gmail.com>
2020-10-23 14:34:02 +0100

    correct project

--- src/app/scrumdata.service.ts
+++ src/app/scrumdata.service.ts
@@ -12,7 +12,7 @@ export class ScrumdataService {
 
   _url = 'https://liveapi.chatscrum.com/scrum/api/scrumusers/'
   _loginUrl = 'https://liveapi.chatscrum.com/scrum/api-token-auth/';
-  _scrumProjectUrl = 'https://liveapi.chatscrum.com/scrum/api/scrumprojects/project_id';
+  _scrumProjectUrl = 'https://liveapi.chatscrum.com/scrum/api/scrumprojects/';
 
   public httpOptions = {
     headers : new HttpHeaders({'Content-Type' : 'application/json'})
